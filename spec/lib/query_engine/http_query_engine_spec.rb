18e13068234007287add3d949e801f0cd509aeba
bobba surendranath chowdary <bobba.surendra@gmail.com>
2017-02-06 11:32:16 +0100

    HttpQuery engine now supports filters They replace values not the whole json value

--- spec/lib/query_engine/http_query_engine_spec.rb
+++ spec/lib/query_engine/http_query_engine_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe HttpQueryEngine do
-  let(:qe) { HttpQueryEngine.new("test_client", { "http_host" => "localhost", "http_port" => "9000"}) }
+  let(:qe) { HttpQueryEngine.new("test_client", { "http_host" => "localhost", "http_port" => "9000", "http_query_string" => "predict"}) }
   let(:summary) do
     <<-SUMMARY
     {
@@ -20,15 +20,50 @@ describe HttpQueryEngine do
       { predict: query_params }
     end
 
+    let(:replace_query) do
+    <<-REQ.gsub(/^ {4}/, "")
+      { input_string: "#_replace_f1_#" }
+    REQ
+    end
+
+    let(:replace_query_result) do
+    <<-REQ.gsub(/^ {4}/, "")
+      { input_string: "nice" }
+    REQ
+    end
+
+  let(:filter_array) do
+    {
+      "replace_f1" => [
+        { operator: "", value: "nice", field: "", value_type: "string" }
+      ]
+    }
+  end
     it { expect(qe.decorate(query, nil, query_params)).to include(query_res) }
+
+    it "should replace query with filter value" do
+      expect(qe.decorate(replace_query, filter_array, nil)).to eq replace_query_result
+    end
+
   end
 
   context "explain" do
     let(:query) do
       { predict: { input_text: "je suis content" } }
     end
+
+    let(:replace_query) do
+      { input_string: "#_replace_f1_#" }
+    end
+
     let(:info) do
       { module_name: "module1", method_name: "method1", query_object_name: "qo1", updated_at: Time.new(2015, 03, 17, 12, 41, 36) }
+    end
+
+    it "should make post query and return result" do
+      WebMock.stub_request(:post, "http://localhost:9000/predict").to_return(body: summary, status: 200)
+      qe.connect
+      expect(qe.explain(replace_query, info)).to eq(JSON.parse(summary))
     end
 
     it "should return prediction" do
