2bbdee74b3fe7850bab6315be95497356e6f1024
bobba surendranath chowdary <bobba.surendra@gmail.com>
2018-05-22 16:08:17 +0200

    Bugfix for choosing database where we symbolize the keys

--- lib/query_engine/default_query_engine.rb
+++ lib/query_engine/default_query_engine.rb
@@ -2,7 +2,8 @@ class DefaultQueryEngine
   def initialize(client, settings)
     # settings.keys.each { |key| settings[key.to_sym] = settings.delete(key) }
     # Using inbuilt deep symbolize method instead of manually converting keys to symbols, as the manual method doesn't convert nested keys to symbols
-    @settings = settings.deep_symbolize_keys
+    settings = settings.deep_symbolize_keys
+    @settings = settings
     @client = client
     @database = settings.delete(:database) || client
     @logger = logger
