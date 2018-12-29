810ec10f42744d5b91c8fe8fc44118334300d2a2
hupiv <vincent.moreno@hupi.fr>
2018-12-29 15:52:18 +0100

    #HDR-60 Support pour leaflet des Polygon

--- lib/export/leaflet.rb
+++ lib/export/leaflet.rb
@@ -13,7 +13,13 @@ module Export
         tmp2[k] = row[k]
       end
       # End properties
-      tmp[row["layer_name"]][:features] << { type: row["type"], geometry: { type: row["geometry_type"], coordinates: [row["lng"], row["lat"]] }, properties: tmp2 }
+      if row["geometry_type"] == "Point"
+        tmp_coordinates = [row["lng"], row["lat"]]
+      else
+        tmp_coordinates = row["coordinates"]        
+      end
+      #tmp[row["layer_name"]][:features] << { type: row["type"], geometry: { type: row["geometry_type"], coordinates: [row["lng"], row["lat"]] }, properties: tmp2 }
+      tmp[row["layer_name"]][:features] << { type: row["type"], geometry: { type: row["geometry_type"], coordinates: tmp_coordinates }, properties: tmp2 }
     end
     tmp
   end
