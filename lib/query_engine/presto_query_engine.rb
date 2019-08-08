6b59e7f539cf4aafa4518da04bbce8764370b7c1
Cyril_cr <cyril.crocquesel@hupi.fr>
2019-08-08 17:47:11 +0200

    #SEIN-173 correctif merge

--- lib/query_engine/presto_query_engine.rb
+++ lib/query_engine/presto_query_engine.rb
@@ -6,12 +6,8 @@ require "lib/core_extensions/json/decode" # For json parsing
 class PrestoQueryEngine < DefaultQueryEngine
   def connect
     @settings[:database] = @database
-<<<<<<< HEAD
     @settings[:server] = @settings[:hosts]
     @connexion = Presto::Client.new(@settings)
-=======
-    @connexion = Presto::Client.new(server: @settings[:hosts], @settings)
->>>>>>> 8b7b0b0646d70ab1f5c9075d7c09bca64d2273ce
   end
 
   def execute(query, _)
