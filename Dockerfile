69505f4ae0c275821ee56d3e6b3b42dc70ef58e6
Michel Desconnets <michel.desconnets@gmail.com>
2023-06-19 17:21:06 +0200

    modif

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,21 @@
 FROM ruby:2.2-alpine as build
 RUN apk update && apk add --no-cache nodejs make gcc musl-dev g++ mariadb-dev postgresql-dev mariadb  postgresql 
 
-COPY ./ /app/
-
+RUN adduser -D -H -h /app hupi
+USER hupi
+COPY --chown=hupi ./ /app/
 WORKDIR /app/
 
 RUN bundle install --path vendor/bundle --without development --standalone --deployment --binstubs vendor/bin/
 
+FROM ruby:2.2-alpine as run
+RUN apk update && apk add --no-cache nodejs mariadb  postgresql  mariadb-dev
+RUN adduser -D -H -h /app hupi
+USER hupi
+COPY --from=build --chown=hupi /app/ /app/
+COPY --from=build /usr/local/bundle/config /usr/local/bundle/config
 HEALTHCHECK --interval=10s --timeout=5s --retries=30 CMD nc -z localhost 4000 || exit 1
+
+WORKDIR /app/
 EXPOSE 4000
 CMD /app/start.sh
--- cicd/test.sh
+++ cicd/test.sh
@@ -1,3 +1,16 @@
 #!/bin/bash -x
 # BASE COMMUNE A TOUS LES PROJETS
+cd $(dirname $0)/..
+v=$(git describe --tags --long 2>/dev/null || echo "0.0.0")-${BUILD_NUMBER:-"XX"}
+docker_image=hupi/dataretriever:$v
+
+#TEST UNITAIRE (resultat attendues dans ./cicd/TEST/*xml )
+
+#SONAR
+
+
+#SECU
+[ ! -e /tmp/ansi2html.sh ] && wget https://raw.githubusercontent.com/pixelb/scripts/master/scripts/ansi2html.sh -O /tmp/ansi2html.sh && chmod a+x /tmp/ansi2html.sh
+docker run --rm  -v /var/run/docker.sock:/var/run/docker.sock  goodwithtech/dockle $dockle_option  $docker_image | bash /tmp/ansi2html.sh >  cicd/TARGET/reports/03-DOCKLE.html
+docker run --rm  -v $PWD:/myapp -v /var/run/docker.sock:/var/run/docker.sock  aquasec/trivy image  $docker_image | bash /tmp/ansi2html.sh >  cicd/TARGET/reports/04-TRIVY.html
 
